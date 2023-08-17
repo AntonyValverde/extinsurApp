455a829c832d3bac584772369dc0574fd6846624
AntonyValverde <gatovalverde4@gmail.com>
2023-08-16 23:52:25 -0600

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import{ FaRegUserCircle, FaFire, FaMapMarked, FaTools } from 'react-icons/fa'
+import { FaRegUserCircle, FaFire, FaMapMarked, FaTools, FaSistrix, FaFacebook, FaInstagram, FaEnvelopeOpenText, FaWhatsapp, FaPortrait, FaIcons } from 'react-icons/fa'
 
 export default function Home() {
   return (
@@ -12,12 +12,12 @@ export default function Home() {
 
             <div className="contenedorBusqueda">
               <input className="barraBusqueda" type="text" />
-              <img src="/indexInicio/lupa.png" alt="lupa" className="lupa" />
+              <FaSistrix className="lupa" ></FaSistrix>
             </div>
 
             <nav className="navegation">
               <Link href="/IndexInicioSesion" className="buttonBorder">
-                Iniciar Secion.
+                <span className="text">Iniciar Secion</span>
                 <FaRegUserCircle className="icons"></FaRegUserCircle>
               </Link>
             </nav>
@@ -33,7 +33,7 @@ export default function Home() {
 
         <nav className="navInfo">
           <h1 className="h1Info">
-            Extinsur.
+          <span className="text">Extinsur</span>
             <FaFire className="icons"></FaFire>
           </h1>
           <div>
@@ -57,7 +57,7 @@ export default function Home() {
 
         <nav className="navDireccion">
           <h1 className="h1Info">
-            Puestos de venta. 
+            <span className="text">Puestos de venta</span>
             <FaMapMarked className="icons"></FaMapMarked>
           </h1>
           <div>
@@ -78,8 +78,8 @@ export default function Home() {
 
         <nav className="navServicios">
           <h1 className="h1Info">
-            Servicios.
-            <FaTools className="icons"></FaTools>  
+            <span className="text">Servicios</span>
+            <FaTools className="icons"></FaTools>
           </h1>
           <div>
             <article className="articleInfo">
@@ -119,40 +119,49 @@ export default function Home() {
         </nav>
 
         <footer className="footerInicio">
-          <h1 className="contactos">Contactanos</h1>
           <div>
             <div>
-              <h2>WhatsApp</h2>
+              <h2>Contactos <FaPortrait className="icons"></FaPortrait></h2>
               <div>
-                <h3>8484-3323</h3>
-                <h3>7483-4934</h3>
-              </div>  
+                <h3>8324-4323</h3>
+                <h3>4342-5924</h3>
+              </div>
             </div>
           </div>
 
           <div>
-            <h1>Correos</h1>
+            <div>
+              <h2>WhatsApp <FaWhatsapp className="icons"></FaWhatsapp></h2>
+              <div>
+                <h3>8484-3323</h3>
+                <h3>7483-4934</h3>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h1>Correos <FaEnvelopeOpenText className="icons"></FaEnvelopeOpenText></h1>
             <div>
               <h3>extinsurdigital@gmail.com</h3>
             </div>
           </div>
 
           <div>
-            <h1>Redes Sociales</h1>
+            <h1>Redes Sociales <FaIcons className="icons"></FaIcons></h1>
             <div>
-              <h2>Instagram</h2>
+              <h2>Instagram <FaInstagram className="icons"></FaInstagram></h2>
               <div>
-                
+
                 <a className="linkMaps" href=" https://instagram.com/extinsurextintores?igshid=MzRlODBiNWFlZA== ">extinsurextintores</a>
-                
+
               </div>
-              <h2>Facebook</h2>
+              <h2>Facebook <FaFacebook className="icons"></FaFacebook></h2>
               <div>
                 <a className="linkMaps" href=" https://www.facebook.com/extinsur.extintores ">Extinsur Extintores </a>
               </div>
             </div>
           </div>
-          
+
         </footer>
       </div>
     </>
